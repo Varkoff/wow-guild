6f815f89a1275f53021dcd5502e9fa70ff844003
Varkoff <virgileboris96@hotmail.fr>
2022-06-11 10:16:29 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<section id='welcome'>
+				<div>
+					<h1>Army of Shadows recrute !</h1>
+					<p>Penses-tu être à la hauteur ?</p>
+					<p>
+						Emploi du temps - progression - objectifs - description des membres
+						- ce que vous pouvez faire - fun
+					</p>
+				</div>
+			</section>
+			<div id='effect'></div>
+		</div>
+	);
+}
+
+export default App;
